90348af325519a07716f808bdf7144cecaeae5f0
Kohei Yamaguchi <fix7211@gmail.com>
2019-03-19 00:39:29 +0900

    Add driver.

--- toy.cpp
+++ toy.cpp
@@ -361,6 +361,28 @@ static std::unique_ptr<FunctionAST> ParseTopLevelExpr() {
     return nullptr;
 }
 
+// top ::= definition | external | expression | ';'
+static void MainLoop() {
+    while(1) {
+        fprintf(stderr, "ready> ");
+        swich(CurTok) {
+        case tok_eof:
+            return;
+        case ';': // ignore top-level semicolons.
+            getNextToken();
+            break;
+        case tok_def:
+            HandleDefinition();
+            break;
+        case tok_extern:
+            HandleExtern();
+            break;
+        default:
+            HandleTopLevelExpression();
+            break;
+        }
+    }
+}
 
 //
 // Main driver code.
