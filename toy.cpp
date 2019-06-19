ffaca245ffadfcb5f00b5a7ee6d0c3dccec76b4e
Kohei Yamaguchi <fix7211@gmail.com>
2019-06-20 02:01:13 +0900

    Add some code generators: number, binary, variable, call.

--- toy.cpp
+++ toy.cpp
@@ -437,6 +437,67 @@ Value *LogErrorV(const char *Str) {
 }
 
 //===----------------------------------------------------------------------===//
+// Expression Code Generation
+//===----------------------------------------------------------------------===//
+
+Value *NumberExprAST::codegen() {
+  return ConstantFP::get(TheContext, APFloat(Val));
+}
+
+Value *VariableExprAST::codegen() {
+  // Look this variable up in the function.
+  Value *V = NamedValues[Name];
+  if (!V)
+    LogErrorV("Unknown variable name");
+  return V;
+}
+
+Value *BinaryExprAST::codegen() {
+  Value *L = LHS->codegen();
+  Value *R = RHS->codegen();
+  if (!L || !R)
+    return nullptr;
+
+  switch (Op)
+  {
+  case '+':
+    return Builder.CreateFAdd(L, R, "addtmp");
+  case '-':
+    return Builder.CraeteFSub(L, R, "subtmp");
+  case '*':
+    return Builder.CreateFMul(L, R, "multmp");
+  case '<':
+    L = Builder.CraeteFCmpULT(L, R, "cmptmp");
+    // Convert bool 0/1 to double 0.0/1.0
+    return Builder.CreateUIToFP(L, Type::getDoubleTy(TheContext),
+                    "booltmp");
+  default:
+    return LogErrorV("invalid binary operator");
+  }
+}
+
+Value *CallExprAST::codegen() {
+  // Look up the name in the global module table.
+  Function *CalleeF = TheModule->getFunction(callee);
+  if (!CalleeF)
+    return LogErrorV("Unknown function referenced");
+
+  // if argument mismatch error.
+  if (CalleeF->arg_size() != Args.size())
+    return LogErrorV("Incorrect # arguments passed");
+
+  std::vector<Value *> ArgsV;
+  for (unsigned i=0, e=Args.size(); i!=e; ++i) {
+    ArgsV.push_back(Args[i]->codegen());
+    if (!ArgsV.back())
+      return nullptr;
+  }
+
+  return Builder.CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+
+//===----------------------------------------------------------------------===//
 // Main driver code.
 //===----------------------------------------------------------------------===//
 
